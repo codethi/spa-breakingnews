101a5b5b4a5dae855207162d5cf45f1d0429388c
codethi <docencia.thiago@gmail.com>
2022-06-03 21:17:08 -0300

    add footer

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./pages/Home/Home";
 import NewsDetails from "./pages/NewsDetails/NewsDetails";
@@ -15,7 +16,7 @@ export function AppRoutes() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/news-details/:id" element={<NewsDetails />} />
       </Routes>
-      {/* <Footer /> */}
+      {<Footer />}
     </Router>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        Feito com muito carinho por{" "}
+        <a target="_blank" href="https://thicode.netlify.app/">Thi Code</a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
